27c0a8ac6d27cef6c931a63fa4364189398c35ca
rosam <rosam@LAPTOP-J9NR809C>
2022-06-09 15:48:34 +0200

    Aggiunto lombok

--- src/main/java/com/acme/Milazzo/Pizza.java
+++ src/main/java/com/acme/Milazzo/Pizza.java
@@ -1,5 +1,12 @@
 package com.acme.Milazzo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pizza {
 
 private String descrizione;
