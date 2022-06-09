5286e13f8eaea53fbc3cd338e38b68cd25a3b7df
arist <arist@ARISTIDE.homenet.telecomitalia.it>
2022-06-09 15:51:33 +0200

    Commit #3

---
+++ src/main/java/com/acme/aristide/Automobile.java
@@ -0,0 +1,14 @@
+package com.acme.aristide;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Automobile {
+	private String nome;
+	private String modello;
+}
