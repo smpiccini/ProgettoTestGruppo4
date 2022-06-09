208548d35f2ec837cae0120ca73702ae4812b6c6
Ste <Ste@DESKTOP-FMHT9OI>
2022-06-09 15:52:57 +0200

    Aggiunta 3 Ste

--- src/main/java/com/acme/AramuScognamiglio/AramuScognamiglio.java
+++ src/main/java/com/acme/AramuScognamiglio/AramuScognamiglio.java
@@ -1,8 +1,16 @@
 package com.acme.AramuScognamiglio;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AramuScognamiglio {
 	private String nome;
 	private String cognome;
 	
+	//CLASSE PROVA GITHUB UPDATE NEW PROJECT
 
 }
