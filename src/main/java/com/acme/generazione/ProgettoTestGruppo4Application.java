f79aab0288fbc79f46eca6a8158dda82a3b854e2
samue <samue@192.168.1.227>
2022-06-09 15:28:48 +0200

    First commit

---
+++ src/main/java/com/acme/generazione/ProgettoTestGruppo4Application.java
@@ -0,0 +1,13 @@
+package com.acme.generazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoTestGruppo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoTestGruppo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/generazione/ProgettoTestGruppo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.generazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoTestGruppo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
